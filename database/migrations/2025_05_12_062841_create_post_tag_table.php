78c7cc63417d86044f506a0fbd2025e7b85ef9d3
tqt97 <kutuanonline199@gmail.com>
2025-05-13 21:57:27 +0700

    Fix review

--- database/migrations/2025_05_12_062841_create_post_tag_table.php
+++ database/migrations/2025_05_12_062841_create_post_tag_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('post_tag', function (Blueprint $table) {
-            $table->id();
             $table->foreignId('post_id')->constrained('posts')->cascadeOnDelete();
             $table->foreignId('tag_id')->constrained('tags')->cascadeOnDelete();
             $table->primary(['post_id', 'tag_id']);
--- resources/js/admin/posts/create.js
+++ resources/js/admin/posts/create.js
@@ -1,12 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const select = document.getElementById('tags');
+import initializeTagNoneOptionHandler from '../../common/tag-handler.js';
 
-    select.addEventListener('change', function () {
-        const noneOption = Array.from(select.selectedOptions).find(opt => opt.value === '__none__');
-        if (noneOption) {
-            Array.from(select.options).forEach(option => {
-                option.selected = false;
-            });
-        }
-    });
+document.addEventListener('DOMContentLoaded', function () {
+    initializeTagNoneOptionHandler('tags');
 });
